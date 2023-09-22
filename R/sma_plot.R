1e487b00c3feaff7e6b239f20afd37fd0f92ed7a
wangtien <r05621114@ntu.edu.tw>
2023-09-22 17:17:10 +0200

    let sma_plot have input without quote

--- R/sma_plot.R
+++ R/sma_plot.R
@@ -5,10 +5,10 @@
 #'
 #' @keywords standardised major axis
 #'
-#' @param data a dataframe, include columns x and y
-#' @param x_string string for colmun name x
-#' @param y_string string for column name y
-
+#' @param datf a dataframe, include columns x and y
+#' @param xvar unquoted string for column name x
+#' @param yvar unquoted string for column name y
+# @param colorvar unquoted string for column name that used for coloring
 #' @return a ggplot with regression line and r2 text
 #'
 #' @author Tien-Cheng Wang
@@ -16,7 +16,7 @@
 #' @importFrom smatr sma
 #' @import ggplot2
 #' @importFrom ggpp geom_text_npc
-#' @importFrom purrr map_dfr
+#' @import rlang
 #' @importFrom usethis use_pipe
 #' @importFrom Rdpack reprompt
 #' @export
@@ -24,25 +24,35 @@
 #\insertRef{warton2012}{toolPhD}
 #'
 #' @examples
-#' sma_plot(mtcars,x_string="mpg",y_string = "cyl")
+#' sma_plot(mtcars,xvar=mpg,yvar = cyl)
+# sma_plot(mtcars,xvar=mpg,yvar = cyl,colorvar=cyl)
 # function ----------------------------------------------------------------
-sma_plot <- function(data,x_string,y_string){
+sma_plot <- function(datf,xvar,yvar){
+
+  library(rlang)
   # sma plot
   # note that this is not yet applicable to groups.
-  SMA <-  smatr::sma(paste0(y_string,"~",x_string), method = "SMA", data = data)
+  SMA <-  smatr::sma(paste0(substitute(yvar),"~",substitute(xvar)), method = "SMA", data = datf)
   SMA.slope <- coef(SMA)[[2]]
   SMA.intercept <- coef(SMA)[[1]]
   r2 <- paste("italic(y)==",format(SMA.slope, digits = 2),
               "%.%italic(x)","+", format(SMA.intercept, digits = 2),
               "~~~italic(r)^2==",format(SMA$r2, digits = 2),sep = "")
 
-  ggplot(data=data) +
-    geom_point(aes(x=!!sym(x_string),y=!!sym(y_string)), size=3,show.legend=F)+
+  p <- ggplot(data=datf) +
+    geom_point(aes(x={{xvar}},y={{yvar}}), shape=1,size=3,show.legend=F)+
     geom_abline(aes(slope=SMA.slope,intercept=SMA.intercept),
                 size=1.25,show.legend=F)+
     ggpp::geom_text_npc(aes(npcx="right",npcy="bottom",label =r2),
                         parse = T,size=4)+
-    theme_test()+
-    theme(axis.text = element_text(size=12,face = 2),
-          axis.title=element_text(size=15,face=2))
+    theme_phd_facet()
+  # not successful
+  # colorvar=NULL
+  # if(!is.null(colorvar)){
+  #   p <- p+
+  #     geom_point(data=datf,
+  #                mapping=aes(x={{xvar}},y={{yvar}},color=as.factor({{colorvar}})), shape=1,size=3)
+  # }
+  return(p)
 }
+
